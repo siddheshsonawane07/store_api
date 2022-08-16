6d896276530b702dc0395c9535d6780791093fc9
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-08-16 21:39:03 +0530

    mongoose schema

--- models/product.js
+++ models/product.js
@@ -5,6 +5,30 @@ const productSchema = new mongoose.Schema({
     type: String,
     required: [true, "product name must be provided"],
   },
-  price: { type: Number, required: [true, "product price must be provided"] },
-  featured: { type: Boolean, default: false },
+  price: {
+    type: Number,
+    required: [true, "product price must be provided"],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ["ikea", "liddy", "caressa", "marcos"],
+      message: `{VALUE} is not supported`,
+    },
+  },
+  //   company: { type: String, enum: ["ikea", "liddy", "caressa", "marcos"] },
 });
+
+module.exports = mongoose.model("Product", productSchema);
