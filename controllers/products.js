8038c6dc84bb5eb605dcac6ed01ccbce2011a510
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-08-16 21:20:46 +0530

    error async package

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,10 @@
 const getAllproductsStatic = async (req, res) => {
+    throw new Error('testing async errors')
     res.status(200).json({ msg: "product testing route" })
 }
 
 const getAllproducts = async (req, res) => {
-    res.status(200).json({ msg: "product testing route" })
+    res.status(200).json({ msg: "product route" })
 }
 
 module.exports = {
