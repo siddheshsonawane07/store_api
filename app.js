6f632d24058e0156322a3d8e347607329b7021b6
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-08-20 21:09:17 +0530

    setting up public and index.html

--- app.js
+++ app.js
@@ -11,13 +11,12 @@ const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
+app.use(express.static("public"));
 
 //routes
 
 app.get("/", (req, res) => {
-  res.send(
-    `<h1> Store API </h1> <a href = "/api/v1/products"> products route </a>`
-  );
+  res.send('index');
 });
 
 app.use("/api/v1/products", productsRouter);
